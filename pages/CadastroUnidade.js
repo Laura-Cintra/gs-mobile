9d161a22c7b085efdcfe330b3254675262c54c45
Laura Cintra <lauraocintra@gmail.com>
2025-05-31 16:47:58 -0300

    feat: unit form

--- pages/CadastroUnidade.js
+++ pages/CadastroUnidade.js
@@ -1,8 +1,15 @@
 import { StyleSheet, View } from 'react-native';
 import StepCircle from '../components/Formulario/StepCircle';
 import FormTitle from '../components/Formulario/Title';
+import InputText from '../components/Formulario/InputText';
+import Button from '../components/Formulario/Button';
+import colors from '../theme/colors';
+import { useState } from 'react';
 
 export default function CadastroUnidade({ navigation, route }) {
+  const [nome, setNome] = useState('');
+  const [capacidade, setCapacidade] = useState('');
+  const [apartamentos, setApartamentos] = useState('');
 
   const handleNext = () => {
     navigation.navigate('CadastroEndereco', {
@@ -17,6 +24,15 @@ export default function CadastroUnidade({ navigation, route }) {
         <FormTitle subtitle="Unidade"/>
         <StepCircle currentStep={2} />
       </View>
+
+      <InputText label="Nome" placeholder="Insira o nome do seu prédio" value={nome} onChangeText={setNome} />
+      <InputText label="Capacidade Reservatório (mL)" placeholder="Ex.: 10000" value={capacidade} onChangeText={setCapacidade} keyboardType="numeric" />
+      <InputText label="Quantidade de apartamentos" placeholder="Ex.: 20" value={apartamentos} onChangeText={setApartamentos} keyboardType="numeric" />
+
+      <View style={styles.button}>
+        <Button title="Limpar" backgroundColor={colors.lightSecondary} onPress={() => { setNome(''); setEmail(''); setSenha(''); }} />
+        <Button title="Enviar" backgroundColor={colors.primary} onPress={handleNext} />
+      </View>
     </View>
   );
 }
@@ -25,5 +41,10 @@ const styles = StyleSheet.create({
   container: {
     paddingVertical: 20,
     paddingHorizontal: 35,
+  },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20
   },
 });
