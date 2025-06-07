b4c8111f79c012a6cc27afd584a97de364a69566
Laura Cintra <lauraocintra@gmail.com>
2025-06-07 00:43:07 -0300

    feat: api url

--- services/actions.js
+++ services/actions.js
@@ -1,5 +1,5 @@
 // const API_URL = 'http://localhost:8080';
-const API_URL = "https://maisagua-api.onrender.com/";
+const API_URL = "https://maisagua-api.onrender.com";
 
 // Função pra tratar erro
 const handleError = async (response) => {
